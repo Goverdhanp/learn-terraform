9fae1095897bb66a007842930102387a588a29ab
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-05-22 15:52:59 +0530

    ss

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -2,3 +2,21 @@ resource "local_file" "foo" {
   content  = "helloo"
   filename = "/tmp/foo.bar"
 }
+
+# normal variable
+variable "x"{
+    default=10
+}
+
+#print variable on output
+
+output "x" {
+    value=var.x
+}
+
+# if our value is just variable then var.x, if it is combination of strings then "${var.x}
+
+output "x1"{
+    value=" value of x - ${var.x}"
+}
+
